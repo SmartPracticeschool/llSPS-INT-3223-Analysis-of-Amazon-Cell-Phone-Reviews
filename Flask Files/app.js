2a4bad5c3d4e19b27644c1c57bc921063a9c5677
AnanthaPriya <51704455+AnanthaPriya@users.noreply.github.com>
2020-07-07 10:46:07 +0530

    Update app.js

--- Flask Files/app.js
+++ Flask Files/app.js
@@ -1,14 +1,16 @@
 
 const textarea = document.querySelector('textarea');
 const submit = document.querySelector('button');
-
+const resultdiv = document.getElementById('result');
 submit.addEventListener('click',(event) => {
 	const value =  textarea.value;
 	if(value != ''){
-		axios.post('https://localhost:5000/predict',{
+		axios.post('http://localhost:5000/predict',{
 			tweet : value
 		}).then(response => {
-			console.log(response.data);
+			resultdiv.innerHTML = `<p>${response.data}</p>`
+			resultdiv.classList.remove('positive','negative');
+			resultdiv.classList.add(response.data == 'Positive' ? 'positive' : 'negative');
 		}).catch(err =>{
 			console.log(err);
 		})
